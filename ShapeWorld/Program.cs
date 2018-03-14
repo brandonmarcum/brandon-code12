cdd2bb8f5739d8b150622818c91cc98ddfc663e0
Brandon Marcum <brandon.marcum9@gmail.com>
2018-03-14 11:42:06 -0400

    2nd commit

--- ShapeWorld/Program.cs
+++ ShapeWorld/Program.cs
@@ -9,8 +9,14 @@ namespace ShapeWorld
             Console.WriteLine("Hello World!");
             
 
-            Shape shape = new Shape();
+            Shape shape = new Shape(5);
+            ClassLibrary1.Class1 class1 = new ClassLibrary1.Class1();
             Console.WriteLine(shape.hello);
+            shape.SetHello("12346");
+            shape.Hello = "50";
+            Console.WriteLine(shape.Hello);
+            GC.Collect(3,GCCollectionMode.Forced);
+
 
             Console.ReadKey();
         }
--- ShapeWorld/Shape.cs
+++ ShapeWorld/Shape.cs
@@ -3,16 +3,49 @@ using si = System.String;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShapeWorld.Library
+namespace ShapeWorld
 {
     class Shape
     {
+
         //fields
         public string hello = "hello";
+        private int _edge = 1;
 
         //properties
+        public int Edge
+        {
+            get
+            {
+                return _edge;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+                _edge = value;
+            }
+        }
+        public string Hello { get; set; }
         //constructors
+        private Shape()
+        {
+            //common setup
+            hello = "shape";
+            Hello = "shape-shifter";
+        }
+        public Shape(int sides) : this()
+        {
+            Edge = sides;
+        }
+
         //destructors
         //methods
+        public void SetHello(string h)
+        {
+            hello = h;
+        }
     }
 }

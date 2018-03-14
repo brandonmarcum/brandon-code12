603fb365ce2ed7b755766e1504c63a490fa26f9f
Brandon Marcum <brandon.marcum9@gmail.com>
2018-03-14 11:41:45 -0400

    add gitignore file.

---
+++ ShapeWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ShapeWorld
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+            Shape shape = new Shape();
+            Console.WriteLine(shape.hello);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShapeWorld/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using si = System.String;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeWorld.Library
+{
+    class Shape
+    {
+        //fields
+        public string hello = "hello";
+
+        //properties
+        //constructors
+        //destructors
+        //methods
+    }
+}
